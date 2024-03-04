b47c76fcc50a24558b009f06870a83aabf60a18b
Polaris-algedi <karchalmohammed@gmail.com>
2024-03-04 21:03:16 +0000

    Add task9 solution

---
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -0,0 +1,18 @@
+/* ES6 method properties */
+import getBudgetObject from './7-getBudgetObject.js';
+
+export default function getFullBudgetObject(income, gdp, capita) {
+  const budget = getBudgetObject(income, gdp, capita);
+
+  const fullBudget = {
+    ...budget,
+    getIncomeInDollars(income) { // ES6 method property syntax
+      return `$${income}`;
+    },
+    getIncomeInEuros(income) { // ES6 method property syntax
+      return `${income} euros`;
+    },
+  };
+
+  return fullBudget;
+}
