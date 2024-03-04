6077e2ff3b8fddc779da4cdd551e7223a47ce118
Polaris-algedi <karchalmohammed@gmail.com>
2024-03-04 20:08:49 +0000

    Add task8 solution

---
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -0,0 +1,15 @@
+/* No need to create empty objects before adding in properties */
+function getCurrentYear() {
+  const date = new Date();
+  return date.getFullYear();
+}
+
+export default function getBudgetForCurrentYear(income, gdp, capita) {
+  const budget = {
+    [`income-${getCurrentYear()}`]: income,
+    [`gdp-${getCurrentYear()}`]: gdp,
+    [`capita-${getCurrentYear()}`]: capita,
+  };
+
+  return budget;
+}
