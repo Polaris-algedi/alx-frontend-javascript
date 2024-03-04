b602bce387f32bba7fea5f96c054826733da8ff8
Polaris-algedi <karchalmohammed@gmail.com>
2024-03-04 23:47:20 +0000

    Add tas12 solution

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,9 @@
+/* Let's create a report object */
+export default function createReportObject(employeesList) {
+  return {
+    allEmployees: employeesList,
+    getNumberOfDepartments() {
+      return Object.keys(this.allEmployees).length;
+    },
+  };
+}
