fab7d444b0637e88d02a64f87f10bfa22947b756
Polaris-algedi <karchalmohammed@gmail.com>
2024-03-04 21:49:34 +0000

    Add tas10 solution

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,9 @@
+/* For...of Loops */
+export default function appendToEachArrayValue(array, appendString) {
+  const newArray = [];
+  for (const element of array) {
+    newArray.push(appendString + element);
+  }
+
+  return newArray;
+}
