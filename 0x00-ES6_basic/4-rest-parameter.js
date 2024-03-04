26504e3b23206a67c92d2698d86d5d55981fcf5c
Polaris-algedi <karchalmohammed@gmail.com>
2024-03-04 18:07:55 +0000

    Fix eslint erorr

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,14 +1,5 @@
 /* Rest parameter syntax for functions */
 
-/*
- * The easiest solution:
- *
- * export default function returnHowManyArguments(...args) {
-  return args.length;
-} */
-
 export default function returnHowManyArguments(...args) {
-  let sum = 0;
-  for (const arg of args) sum += 1;
-  return sum;
+  return args.length;
 }
