bc5539eab5b17281edbe2ed6955b0a7a9b8e47a8
Polaris-algedi <karchalmohammed@gmail.com>
2024-03-05 15:46:05 +0000

    Add a function that returns a promise based on the provided boolean argument

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,15 @@
+/* Returns a promise based on the provided boolean argument */
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      // Resolve the promise with a success object.
+      resolve({
+        status: 200,
+        body: 'Success',
+      });
+    } else {
+      // Reject the promise with an error object.
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+}
