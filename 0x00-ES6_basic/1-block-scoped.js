70935ebf798b3d08719b91575827a91179267489
Polaris-algedi <karchalmohammed@gmail.com>
2024-03-05 01:03:40 +0000

    Fix eslint no-unused-vars error

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,39 +1,12 @@
-/*
-Given what you’ve read about var and hoisting,
-modify the variables inside the function taskBlock so that the variables aren’t
-overwritten inside the conditional block.
-
-export default function taskBlock(trueOrFalse) {
-  var task = false;
-  var task2 = true;
-
-  if (trueOrFalse) {
-    var task = true;
-    var task2 = false;
-  }
-
-  return [task, task2];
-}
-
-Execution:
-
-bob@dylan:~$ cat 1-main.js
-import taskBlock from './1-block-scoped.js';
-
-console.log(taskBlock(true));
-console.log(taskBlock(false));
-bob@dylan:~$
-bob@dylan:~$ npm run dev 1-main.js
-[ false, true ]
-[ false, true ]
-bob@dylan:~$
-*/
+/* Block Scope */
 export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
   if (trueOrFalse) {
+    // eslint-disable-next-line no-unused-vars
     const task = true;
+    // eslint-disable-next-line no-unused-vars
     const task2 = false;
   }
 
