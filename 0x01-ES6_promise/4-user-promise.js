4aa89a328ba670ad9ac6e968d62e6139a65d3905
Polaris-algedi <karchalmohammed@gmail.com>
2024-03-05 21:13:26 +0000

    Add a function that returns a resolved promise with an object containing firstName and lastName

---
+++ 0x01-ES6_promise/4-user-promise.js
@@ -0,0 +1,4 @@
+/* Returns a resolved promise with an object containing firstName and lastName. */
+export default function signUpUser(firstName, lastName) {
+  return Promise.resolve({ firstName, lastName });
+}
