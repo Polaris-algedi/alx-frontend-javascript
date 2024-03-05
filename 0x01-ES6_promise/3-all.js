e54f97d73edd29fc700afe2e47155ce1da89c87f
Polaris-algedi <karchalmohammed@gmail.com>
2024-03-05 20:49:33 +0000

    Collectively resolves promises from uploadPhoto and createUser and logs the result

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,17 @@
+/* Collectively resolves promises from uploadPhoto and createUser and logs the result */
+
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  const photoPromise = uploadPhoto();
+  const userPromise = createUser();
+
+  // Use Promise.all to wait for both promises to resolve
+  Promise.all([photoPromise, userPromise])
+    .then(([photoResult, userResult]) => {
+      console.log(`${photoResult.body} ${userResult.firstName} ${userResult.lastName}`);
+    })
+    .catch(() => {
+      console.error('Signup system offline');
+    });
+}
