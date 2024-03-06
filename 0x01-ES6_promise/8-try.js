e249bbc62123986136b3f8fa3eec7d872a745071
Polaris-algedi <karchalmohammed@gmail.com>
2024-03-06 00:53:31 +0000

    Add a function that throws a new error with the message cannot divide by 0 when the denominator is equal to 0

---
+++ 0x01-ES6_promise/8-try.js
@@ -0,0 +1,8 @@
+/* Divides the numerator by the denominator */
+export default function divideFunction(numerator, denominator) {
+  if (denominator === 0) {
+    throw new Error('cannot divide by 0');
+  }
+
+  return numerator / denominator;
+}
