594d01dea2744e4df08dc7a0aa07c70dc35f2ccc
Polaris-algedi <karchalmohammed@gmail.com>
2024-03-06 17:58:30 +0000

    Fix: Use curly brackets

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -6,9 +6,9 @@ export default function handleProfileSignup() {
 
   // Use Promise.all to wait for both promises to resolve
   return Promise.all([photoPromise, userPromise])
-    .then((res) => (
-      console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`)
-    ))
+    .then((res) => {
+      console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
+    })
     .catch(() => {
       console.error('Signup system offline');
     });
