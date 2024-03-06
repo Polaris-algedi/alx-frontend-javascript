120bcbd578f988cd7d0175c6947480650010f025
Polaris-algedi <karchalmohammed@gmail.com>
2024-03-06 00:50:14 +0000

    Add task6 solution

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,15 @@
+/* Handles the signup process by calling signUpUser and uploadPhoto functions */
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const signUpPromise = signUpUser(firstName, lastName);
+  const uploadPhotoPromise = uploadPhoto(fileName);
+
+  // Use Promise.allSettled to wait for both promises to settle
+  return Promise.allSettled([signUpPromise, uploadPhotoPromise])
+    .then((results) => results.map((result) => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : result.reason,
+    })));
+}
