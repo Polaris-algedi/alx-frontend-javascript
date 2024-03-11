ade0babc501e9a5a360efa8dafbee1c2044fb4bd
Polaris-algedi <karchalmohammed@gmail.com>
2024-03-11 20:32:39 +0000

    Add a function named groceriesList that returns a map of groceries

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,11 @@
+/* Returns a map of groceries */
+
+export default function groceriesList() {
+  return new Map([
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ]);
+}
