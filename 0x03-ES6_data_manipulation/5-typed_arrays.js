61ef4c3121b34d46f8e4cdae9b3214bad0d7eed3
Polaris-algedi <karchalmohammed@gmail.com>
2024-03-11 21:13:49 +0000

    Fix: Return a DataView object

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -5,8 +5,8 @@ export default function createInt8TypedArray(length, position, value) {
     throw new Error('Position outside range');
   }
   const buffer = new ArrayBuffer(length);
-  const int8Array = new Int8Array(buffer);
+  const view = new DataView(buffer);
 
-  int8Array[position] = value;
-  return int8Array;
+  view.setInt8(position, value);
+  return view;
 }
