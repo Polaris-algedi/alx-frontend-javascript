3c4c18f600fd26d31803dd3f6e1fbd8acf21b9d3
Polaris-algedi <karchalmohammed@gmail.com>
2024-03-11 18:12:52 +0000

    Add a function updateStudentGradeByCity that returns an array of students for a specific city with their new grade

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,11 @@
+/* Returns an array of students for a specific city with their new grade */
+
+export default function updateStudentGradeByCity(studentsList, city, newGrades) {
+  return studentsList
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const gradeObj = newGrades.find((grade) => grade.studentId === student.id);
+      const grade = gradeObj ? gradeObj.grade : 'N/A';
+      return { ...student, grade };
+    });
+}
