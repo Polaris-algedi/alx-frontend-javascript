2116e2f1544ad42b626e9fc17eb1f297a3b5412b
Polaris-algedi <102882133+Polaris-algedi@users.noreply.github.com>
2024-12-29 07:02:59 +0100

    Writing a class
    Write a Class named StudentClass:
    
    The constructor accepts firstName(string) and lastName(string) arguments
    The class has a method named workOnHomework that return the string Currently working
    The class has a method named displayName. It returns the firstName of the student
    The constructor of the class should be described through an Interface
    The class should be described through an Interface

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -18,3 +18,47 @@ interface printTeacherFunction {
 const printTeacher: printTeacherFunction = (firstName, lastName) => `${firstName[0]}. ${lastName}`;
 
 console.log(printTeacher("John", "Doe")); // Output: J. Doe
+
+
+// 4. Writing a class
+/*
+Write a Class named StudentClass:
+
+The constructor accepts firstName(string) and lastName(string) arguments
+The class has a method named workOnHomework that return the string Currently working
+The class has a method named displayName. It returns the firstName of the student
+The constructor of the class should be described through an Interface
+The class should be described through an Interface
+*/
+
+
+
+
+interface StudentClassConstructor {
+  new (firstName: string, lastName: string): StudentClass
+}
+
+interface Student {
+  firstName: string;
+  lastName: string;
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+
+class StudentClass implements Student {
+  firstName: string
+  lastName: string
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName
+    this.lastName = lastName
+  }
+  
+  workOnHomework(): string {
+    return "Currently working"
+  }
+  
+  displayName() {
+    return this.firstName
+  }
+}
